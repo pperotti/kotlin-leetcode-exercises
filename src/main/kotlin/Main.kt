d469e495d746fd62371ce48581bfbd3832a46f67
Pablo Perotti <pablo.perotti.-ND@disneystreaming.com>
2024-03-20 16:04:16 -0300

    Project: Add first 2 examples for the sliding window class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
-import problems.leetcode.das.arraysnstrings.ProblemSquaresOfSortedArray
+import problems.leetcode.das.arraysnstrings.slidingwindow.Example1
 
 fun main() {
     //ProblemTwoSums().execute()
     //ProblemContainerWithMostWater().execute()
     //ProblemTrappingRainWater().execute()
     //ProblemReverseString().execute()
-    ProblemSquaresOfSortedArray().execute()
+    //ProblemSquaresOfSortedArray().execute()
+    Example1().execute()
 }
---
+++ src/main/kotlin/problems/leetcode/das/arraysnstrings/slidingwindow/Example2.kt
@@ -0,0 +1,19 @@
+package problems.leetcode.das.arraysnstrings.slidingwindow
+
+import problems.Problem
+
+/**
+ * Example 2: You are given a binary string s (a string
+ * containing only "0" and "1"). You may choose up to one "0"
+ * and flip it to a "1". What is the length of the longest
+ * substring achievable that contains only "1"?
+ *
+ * For example, given s = "1101100111", the answer is 5.
+ * If you perform the flip at index 2, the string becomes
+ * 1111100111.
+ */
+class Example2 : Problem {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
