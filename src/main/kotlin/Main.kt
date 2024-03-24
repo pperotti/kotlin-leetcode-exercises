f7af5239dba6d14839381bef7b0dedbc1551bba1
Pablo Perotti <pablo.perotti.-ND@disneystreaming.com>
2024-03-24 18:31:34 -0300

    Project: Add solution to problem: Max Consecutive Ones III

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import problems.leetcode.das.arraysnstrings.ProblemMaxConsecutiveOnesIII
 import problems.leetcode.das.arraysnstrings.ProblemMaximumAverageSubarray
 import problems.leetcode.das.arraysnstrings.slidingwindow.Example2
 import problems.leetcode.das.arraysnstrings.slidingwindow.Example3
@@ -13,21 +14,6 @@ fun main() {
     //Example2().execute()
     //Example3().execute()
     //Example4().execute()
-    ProblemMaximumAverageSubarray().execute()
+    //ProblemMaximumAverageSubarray().execute()
+    ProblemMaxConsecutiveOnesIII().execute()
 }
-/*
-public int findBestSubarray(int[] nums, int k) {
-    int curr = 0;
-    for (int i = 0; i < k; i++) {
-        curr += nums[i];
-    }
-
-    int ans = curr;
-    for (int i = k; i < nums.length; i++) {
-        curr += nums[i] - nums[i - k];
-        ans = Math.max(ans, curr);
-    }
-
-    return ans;
-}
- */
---
+++ src/main/kotlin/problems/leetcode/das/arraysnstrings/ProblemMaxConsecutiveOnesIII.kt
@@ -0,0 +1,103 @@
+package problems.leetcode.das.arraysnstrings
+
+import problems.Problem
+
+/**
+ * Given a binary array nums and an integer k, return the maximum number
+ * of consecutive 1's in the array if you can flip at most k 0's.
+ */
+class ProblemMaxConsecutiveOnesIII : Problem {
+    override fun execute() {
+
+        //val nums = intArrayOf(1,1,0,1,0)
+        //val k = 1
+
+        //val nums = intArrayOf(1,1,1,0,0,0,1,1,1,1,0)
+        //val k = 2
+
+        // nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+        val nums = intArrayOf(0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1)
+        val k = 3
+
+        val answer = longestOnes(nums, k)
+        println("Numbers: ${nums.contentToString()} Size: ${nums.size} K: $k")
+        println("Max: $answer")
+    }
+
+    private fun longestOnes(nums: IntArray, k: Int): Int {
+        var left = 0
+        var right = 0
+        var answer = 0
+        var continueEvaluation = true
+        var numZeros = 0
+
+        //for (times in 0 until nums.size ) {
+        while (continueEvaluation) {
+            println("--> left: $left right: $right numZeros: $numZeros")
+            // Evaluate right pointer
+            if (nums[right]==0) {
+                numZeros++
+                println("-----> It's a zero - numZeros: $numZeros")
+                do {
+                    if (numZeros > k) {
+                        // Move Left Pointer to the right
+                        println("-----> Moving left pointer to left: ${left+1}")
+                        if (nums[left] == 0) {
+                            numZeros--
+                            println("-----> nums[$left] is 0 - numZeros is now $numZeros")
+                        } else {
+                            println("-----> nums[$left] is 1 - numZeros is now $numZeros")
+                        }
+                        left++
+                    }
+                } while (numZeros > k && left <= right)
+                right++
+            } else {
+                right++
+            }
+
+            answer = Math.max(answer, right - left)
+
+            if (right > nums.size - 1) {
+                continueEvaluation = false
+            }
+            println("-----> best answer: $answer")
+        }
+
+        return answer
+    }
+
+    /**
+     * It looks like this solution is O(n^2) in worst case scenario and O(n)
+     */
+    private fun longestOnes2(nums: IntArray, k: Int): Int {
+        var answer = 0
+
+        for (left in 0 until nums.size) {
+            var right = left
+            var numZeros = 0
+            var longestWindow = 0
+
+            //println("Starting at: $right")
+            while (right < nums.size) {
+                if (nums[right] == 0) {
+                    if (numZeros == k) {
+                        break
+                    } else {
+                        numZeros++
+                    }
+                }
+                //println("--> right: $right numZeros: $numZeros")
+                longestWindow++
+                right++
+            }
+
+            answer = Math.max(longestWindow, answer)
+
+            //println("--> Numbers: ${nums.contentToString()} Size: ${nums.size} K: $k")
+            println("--> Loop at index: $left has the best window of size: $longestWindow numZeros=$numZeros")
+        }
+
+        return answer
+    }
+}
