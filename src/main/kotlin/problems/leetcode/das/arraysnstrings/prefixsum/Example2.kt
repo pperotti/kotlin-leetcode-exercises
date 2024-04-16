46ec36f4ccd65e6b492a876ad940b6e28af9e8f7
pablo <pablo.perotti@gmail.com>
2024-04-16 02:25:37 -0300

    Project: Add the first example for prefix sum section

---
+++ src/main/kotlin/problems/leetcode/das/arraysnstrings/prefixsum/Example2.kt
@@ -0,0 +1,24 @@
+package problems.leetcode.das.arraysnstrings.prefixsum
+
+import problems.Problem
+
+/**
+ * This is Example2 shown in Prefix Sum lesson.
+ *
+ * Given an integer array nums, find the number of ways to split the array into two
+ * parts so that the first section has a sum greater than or equal to the sum of the
+ * second section. The second section should have at least one number.
+ */
+class Example2 : Problem {
+    override fun execute() {
+        /*
+        val nums = intArrayOf(1, 6, 3, 2, 7, 2)
+        val queries = arrayOf(intArrayOf(0,3), intArrayOf(2,5), intArrayOf(2,4))
+        val limit = 13
+        val answer = answerQueries(nums, queries, limit)
+        System.out.println("Nums: ${nums.contentToString()} Limit: $limit")
+        System.out.println("Queries: ${queries.contentDeepToString()}")
+        System.out.println("Answer: ${answer.contentToString()}")
+         */
+    }
+}
