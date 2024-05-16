0cab7a79d39bc8fc792522dc262c38f521a6ebb7
Pablo Perotti <pablo.perotti.-ND@disneystreaming.com>
2024-05-16 10:47:43 -0300

    Hashing: Update implementation for Continuous Array

--- src/main/kotlin/problems/leetcode/das/hashing/HashingContinousArray.kt
+++ src/main/kotlin/problems/leetcode/das/hashing/HashingContinousArray.kt
@@ -3,6 +3,8 @@ package problems.leetcode.das.hashing
 import problems.Problem
 import utils.readContentsFromFile
 import java.io.File
+import java.util.Arrays
+import kotlin.math.max
 
 /**
  * Given a binary array nums, return the maximum length of a contiguous
@@ -15,7 +17,7 @@ class HashingContinousArray : Problem {
         //val input = intArrayOf(0, 0, 1, 1, 0) // output 4
         //val input = intArrayOf(0, 1, 0, 1) // output 4
         //val input = intArrayOf(0,0,1,0,0,0,1,1) // output 6
-        //val input = intArrayOf(0,1,1,0,1,1,1,0) // output 4
+        val input = intArrayOf(0,1,1,0,1,1,1,0) // output 4
 
         /*
         val input = intArrayOf(
@@ -46,33 +48,39 @@ class HashingContinousArray : Problem {
             1,1,0,1,1,0,0,0,1,1) // Expected 68
         */
 
-        val input = readContentsFromFile()
+        //val input = readContentsFromFile()
         println("Input: ${input.contentToString()} Len: ${input.size}")
         println("Output: ${findMaxLength3(input)}")
     }
 
-
-
+    /**
+     * This solution does not use hashing as it should but a count variable. The key thing
+     * is to understand how count works. When count repeats while traversing the nums array
+     * it means, you hit a point where 1 and 0 are balanced.
+     *
+     * https://leetcode.com/problems/contiguous-array/editorial/
+     */
     fun findMaxLength3(nums: IntArray): Int {
-        val count = intArrayOf(0, 0)
-        var max = 0
-        val left = 0
-        val right = 0
+        val arr = IntArray((2 * nums.size) + 1)
+        Arrays.fill(arr, -2)
+        arr[nums.size] = -1
+        var maxLen = 0
+        var count = 0
 
-        /*
-        nums.forEachIndexed { index, i ->
-            count[i]++
+        println("Array: ${arr.contentToString()}")
+
+        for (i in 0 until nums.size) {
+            count = count + if (nums[i] == 0) -1 else 1
+            println("i: $i count: $count arr[${count + nums.size}]: ${arr[count + nums.size]} (i - arr[count + nums.size]): ${i - arr[count + nums.size]}")
+            if (arr[count + nums.size] >= -1 ) {
+                maxLen = Math.max(maxLen, i - arr[count + nums.size])
+            } else {
+                arr[count + nums.size] = i
+            }
+            println("---> Arr: ${arr.contentToString()} maxlen: ${maxLen}")
         }
-        */
-        for (outterIndex in 0 until nums.size) {
 
-            println("NEW ITERATION #: $outterIndex")
-
-        }
-
-        println("Max Selected - c[0]=${count[0]} c[1]=${count[1]}")
-
-        return max
+        return maxLen
     }
 
     fun findMaxLength2(nums: IntArray): Int {
