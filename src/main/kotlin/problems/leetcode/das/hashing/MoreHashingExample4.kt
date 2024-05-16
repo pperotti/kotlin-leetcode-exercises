2e96ef15911136565b6aa09ba4f9e15a91c07eeb
Pablo Perotti <pablo.perotti.-ND@disneystreaming.com>
2024-05-16 16:46:19 -0300

    More Hashing: Add example 4

---
+++ src/main/kotlin/problems/leetcode/das/hashing/MoreHashingExample4.kt
@@ -0,0 +1,15 @@
+package problems.leetcode.das.hashing
+
+import problems.Problem
+
+/**
+ * Example 4: 2352. Equal Row and Column Pairs
+ *
+ * Given an n x n matrix grid, return the number of pairs (R, C) where R is a row and C is a column,
+ * and R and C are equal if we consider them as 1D arrays.
+ */
+class MoreHashingExample4 : Problem {
+    override fun execute() {
+
+    }
+}
