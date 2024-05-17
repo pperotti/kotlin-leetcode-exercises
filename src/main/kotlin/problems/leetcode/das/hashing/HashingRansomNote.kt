b829b095bae7579efdfc07a34602b425b2564810
Pablo Perotti <pablo.perotti@gmail.com>
2024-05-17 11:26:58 -0300

    Hashing: Add problem 'Ransom Note'

---
+++ src/main/kotlin/problems/leetcode/das/hashing/HashingRansomNote.kt
@@ -0,0 +1,15 @@
+package problems.leetcode.das.hashing
+
+import problems.Problem
+
+/**
+ * Given two strings ransomNote and magazine, return true if ransomNote
+ * can be constructed by using the letters from magazine and false otherwise.
+ *
+ * Each letter in magazine can only be used once in ransomNote.
+ */
+class HashingRansomNote : Problem {
+    override fun execute() {
+
+    }
+}
