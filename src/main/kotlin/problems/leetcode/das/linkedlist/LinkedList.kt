268671f6e187b08bc508a39d042ce9442c24af68
Pablo Perotti <pablo.perotti@gmail.com>
2024-05-22 09:57:57 -0300

    Linked Lists: Add base classes for Linked List related problems.

---
+++ src/main/kotlin/problems/leetcode/das/linkedlist/LinkedList.kt
@@ -0,0 +1,9 @@
+package problems.leetcode.das.linkedlist
+
+/**
+ * This is a place where helper class are located for LinkedIn-related classes
+ */
+class ListNode(value: Int) {
+    var value: Int = value
+    var next: ListNode? = null
+}
