5455805f7bde562114b22eabedaf02ce1fcd32f3
Pablo Perotti <pablo.perotti.-ND@disneystreaming.com>
2024-07-18 16:37:09 -0300

    Queues: Add problem description

--- src/main/kotlin/problems/leetcode/das/queues/QueuesSlidingWindowMaximum.kt
+++ src/main/kotlin/problems/leetcode/das/queues/QueuesSlidingWindowMaximum.kt
@@ -2,6 +2,16 @@ package problems.leetcode.das.queues
 
 import problems.Problem
 
+/**
+ * 239. Sliding Window Maximum
+ * https://leetcode.com/problems/sliding-window-maximum/description/
+ *
+ * You are given an array of integers nums, there is a sliding window of size k which
+ * is moving from the very left of the array to the very right. You can only see the k
+ * numbers in the window. Each time the sliding window moves right by one position.
+ *
+ * Return the max sliding window.
+ */
 class QueuesSlidingWindowMaximum : Problem {
     override fun execute() {
         scenario2()
