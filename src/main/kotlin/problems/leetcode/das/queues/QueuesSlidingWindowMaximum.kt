691cf71a614c3413d1f6d7be090af33635aed4ec
Pablo Perotti <pablo.perotti@gmail.com>
2024-07-19 10:03:42 -0300

    Queues: Introducing file reading to other examples. Update the solution to an 'still' non-optimal solution

--- src/main/kotlin/problems/leetcode/das/queues/QueuesSlidingWindowMaximum.kt
+++ src/main/kotlin/problems/leetcode/das/queues/QueuesSlidingWindowMaximum.kt
@@ -18,9 +18,10 @@ class QueuesSlidingWindowMaximum : Problem {
     }
 
     private fun scenario1() {
-        val nums = intArrayOf(1,3,-1,-3,5,3,6,7)
+        val nums = utils.readIntArrayContentsFromFile("src/main/resources/slidingwindowmaximum2.txt")
         val k = 3
         println("Input: ${nums.contentToString()}")
+        println("Expected: [3,3,5,5,6,7]")
         println("Output: ${maxSlidingWindow(nums, k).contentToString()}")
     }
 
@@ -28,18 +29,18 @@ class QueuesSlidingWindowMaximum : Problem {
         val nums = utils.readIntArrayContentsFromFile("src/main/resources/slidingwindowmaximum.txt")
         val k = 10007
         println("Input: ${nums.contentToString()}")
-        //println("Output: ${maxSlidingWindow(nums, k).contentToString()}")
+        println("Output: ${maxSlidingWindow(nums, k).contentToString()}")
     }
 
     private fun maxSlidingWindow(nums: IntArray, k: Int): IntArray {
         val answers = IntArray(nums.size - k + 1)
+        val queue = MyWindowedQueue(k)
 
-        for (index in answers.indices) {
-            var windowMax = nums[index]
-//            (1 until k).forEach { windowIndex ->
-//                windowMax = windowMax.coerceAtLeast(nums[index + windowIndex])
-//            }
-            answers[index] = windowMax
+        for (index in nums.size - 1 downTo 0) {
+            queue.add(nums[index])
+            if (index < answers.size) {
+                answers[index] = queue.queueMax
+            }
         }
 
         return answers
@@ -49,35 +50,23 @@ class QueuesSlidingWindowMaximum : Problem {
 /**
  * This queue keep track of the max value of the window and recalculate it while it rotates
  */
-class MyQueue(val windowSize: Int) {
+class MyWindowedQueue(val windowSize: Int) {
 
     val queue = ArrayDeque<Int>(initialCapacity = windowSize)
-    var currentMax = 0
+    var queueMax = 0
+
+    fun size() = queue.size
 
     fun add(value: Int) {
-        if (queue.size < windowSize) {
-            addAndRefresh(value)
-        } else {
-            // Get first in the queue
-            // Check if FIRST is currentMax => recalculate
-            // ELSE remove the element
-            val first = queue.removeFirst()
-            if (first == currentMax) {
-                recalculateMax()
-            } else {
-                addAndRefresh(value)
-            }
-        }
-    }
-
-    private fun addAndRefresh(value: Int) {
-        queue.add(value)
-        currentMax = Math.max(currentMax, value)
-    }
-
-    private fun recalculateMax() {
-        queue.iterator().forEach {
-            currentMax = Math.max(currentMax, it)
+        queue.addFirst(value)
+        //println("---> add: $value - queue.size: ${queue.size} - windowSize: $windowSize queue: ${queue}")
+        if (queue.size > windowSize) {
+            queue.removeLast()
+            queueMax = queue.max()
+            //println("---------> queueMax: $queueMax queue: $queue")
+        } else if (queue.size == windowSize) {
+            queueMax = queue.max()
+            //println("---------> queueMax: $queueMax")
         }
     }
 }
