ef7b19f2fd763b1569b3c598ba53e90a577c62a4
Pablo Perotti <pablo.perotti@gmail.com>
2024-07-22 12:14:37 -0300

    Queues: Add template for problem 'Online Stock Span'

---
+++ src/main/kotlin/problems/leetcode/das/queues/QueuesOnlineStockSpan.kt
@@ -0,0 +1,9 @@
+package problems.leetcode.das.queues
+
+import problems.Problem
+
+class QueuesOnlineStockSpan : Problem {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
